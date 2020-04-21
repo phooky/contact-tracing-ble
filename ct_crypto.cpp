6b169417281e0dde114519b23a20293454dceb92
phooky <phooky@gmail.com>
2020-04-20 22:47:37 -0400

    tidying up messy code

--- ct_crypto.cpp
+++ ct_crypto.cpp
@@ -14,109 +14,93 @@ std::tuple<uint32_t,uint8_t> getDayAndTimeInterval() {
     return std::make_tuple(dayNumber, timeIntervalNumber);
 }
 
-void hmac(uint8_t* key, size_t keylen, uint8_t* msg, size_t msglen, uint8_t* buffer, size_t buflen) {
+class HMAC {
     gcry_mac_hd_t hd;
-    gcry_mac_open(&hd, GCRY_MAC_HMAC_SHA256, 0, NULL);
-    gcry_mac_setkey(hd, key, keylen);
-    gcry_mac_write(hd, msg, msglen);
-    gcry_mac_read(hd,buffer,&buflen);
-    if (buflen != 32) throw std::runtime_error("Unexpected SHA-256 HMAC size");
-    gcry_mac_close(hd);
-}
+public:
+    HMAC(const std::vector<uint8_t>& key) {
+        gcry_mac_open(&hd, GCRY_MAC_HMAC_SHA256, 0, NULL);
+        gcry_mac_setkey(hd, key.data(), key.size());
+    }
+    ~HMAC() {
+        gcry_mac_close(hd);
+    }
 
+    void write(const std::vector<uint8_t>& v) {
+        gcry_mac_write(hd, v.data(), v.size());
+    }
+
+    void write(const std::string& s) {
+        gcry_mac_write(hd, s.data(), s.size());
+    }
+
+    void write(const uint8_t* d, const size_t len) {
+        gcry_mac_write(hd, d, len);
+    }
+
+    std::vector<uint8_t> read(size_t sz) {
+        std::vector<uint8_t> v(sz);
+        gcry_mac_read(hd, v.data(), &sz);
+        if (sz != v.size()) throw std::runtime_error("Unexpected SHA-256 HMAC size");
+        return v;
+    }
+};
 
 
 struct DailyTracingKey;
 struct RollingProximityIdentifier;
 
-struct TracingKey {
+struct TracingKey : public std::vector<uint8_t> {
     const static size_t KEYLEN = 32;
-    uint8_t key[KEYLEN];
 
-    TracingKey(const std::string& path) {
+    TracingKey(const std::string& path) : std::vector<uint8_t>(KEYLEN) {
         std::ifstream f(path);
         if (f) { 
-            f.read((char*)key, KEYLEN);
+            f.read((char*)data(), size());
             f.close();
         } else {
-            gcry_randomize(key, KEYLEN, GCRY_VERY_STRONG_RANDOM);
+            gcry_randomize(data(), size(), GCRY_VERY_STRONG_RANDOM);
             // store to path
             std::ofstream f(path);
-            f.write((char*)key, KEYLEN);
+            f.write((char*)data(), size());
             f.close();
         }
     }
-    
-    DailyTracingKey daily_tracing_key(uint32_t dayNumber);
+    std::vector<uint8_t> daily_tracing_key(uint32_t dayNumber);
 };
 
-struct DailyTracingKey {
-    const static size_t KEYLEN = 16;
-    uint8_t key[KEYLEN];
+std::vector<uint8_t> make_rpi(const std::vector<uint8_t>& dtk, uint8_t timeIntervalNumber);
 
-    RollingProximityIdentifier make_rpi(uint8_t timeIntervalNumber);
-};
-
-struct RollingProximityIdentifier {
-    const static size_t RPILEN = 16;
-    uint8_t rpi[RPILEN];
-};
-
-DailyTracingKey TracingKey::daily_tracing_key(uint32_t dayNumber) {
+std::vector<uint8_t> TracingKey::daily_tracing_key(uint32_t dayNumber) {
     // HKDF (tk , NULL , ( UTF8("CT-DTK") || Di ),16)
-    DailyTracingKey dtk;
 
     // Step 1: extract
-    uint8_t prk[32];
-    uint8_t empty_salt[32] = {};
-    gcry_mac_hd_t hd;
-    gcry_mac_open(&hd, GCRY_MAC_HMAC_SHA256, 0, NULL);
-    gcry_mac_setkey(hd, empty_salt, 32); // IKM is the message, not the key
-    gcry_mac_write(hd, key, 32);
-    size_t buflen = 32;
-    gcry_mac_read(hd,prk,&buflen);
-    if (buflen != 32) throw std::runtime_error("Unexpected SHA-256 HMAC size");
-    gcry_mac_close(hd);
+    HMAC extract_mac(std::vector<uint8_t>(32,0));
+    extract_mac.write(*this);
+    auto prk = extract_mac.read(32);
 
     // Step 2: expand. We only need 16 octets, so this is trivial.
-    gcry_mac_open(&hd, GCRY_MAC_HMAC_SHA256, 0, NULL);
-    gcry_mac_setkey(hd, prk, 32); 
-    gcry_mac_write(hd, "CT-DTK", 6);
+    HMAC expand_mac(prk);
+    expand_mac.write(std::string("CT-DTK"));
     uint32_t dn_le = htole32(dayNumber);
-    gcry_mac_write(hd, (uint8_t*)&dn_le, 4);
+    expand_mac.write((uint8_t*)&dn_le, 4);
     uint8_t end_octet = 0x01;
-    gcry_mac_write(hd, &end_octet, 1);
-    buflen = 32;
-    uint8_t buffer[32];
-    gcry_mac_read(hd,buffer,&buflen);
-    if (buflen != 32) throw std::runtime_error("Unexpected SHA-256 HMAC size");
-    gcry_mac_close(hd);
-    for (auto i = 0; i < 16; i++) dtk.key[i] = buffer[i];
-
-    return dtk;
+    expand_mac.write(&end_octet, 1);
+    return expand_mac.read(16);
 }
 
-RollingProximityIdentifier DailyTracingKey::make_rpi(uint8_t timeIntervalNumber) {
-    RollingProximityIdentifier rpi;
-    gcry_mac_hd_t hd;
-    gcry_mac_open(&hd, GCRY_MAC_HMAC_SHA256, 0, NULL);
-    gcry_mac_setkey(hd, key, KEYLEN); 
-    gcry_mac_write(hd, "CT-RPI", 6);
-    gcry_mac_write(hd, &timeIntervalNumber, 1);
-    size_t buflen = 32;
-    uint8_t buffer[32];
-    gcry_mac_read(hd,buffer,&buflen);
-    if (buflen != 32) throw std::runtime_error("Unexpected SHA-256 HMAC size");
-    gcry_mac_close(hd);
-    for (auto i = 0; i < 16; i++) rpi.rpi[i] = buffer[i];
-
-    return rpi;
+std::vector<uint8_t> make_rpi(const std::vector<uint8_t>& dtk, uint8_t timeIntervalNumber) {
+    HMAC mac(dtk);
+    mac.write(std::string("CT-RPI"));
+    mac.write(&timeIntervalNumber,1);
+    return mac.read(16);
 }
-
 
 int main() {
     TracingKey tk("test.key");
-    for (auto i = 0;i < tk.KEYLEN; i++) std::cout << tk.key[i];
+    auto [ day, time ] = getDayAndTimeInterval();
+    auto dtk = tk.daily_tracing_key(day);
+    auto rpi = make_rpi(dtk, time);
+    for (auto i = 0;i < 16; i++) std::cout << rpi[i];
 }
     
 
