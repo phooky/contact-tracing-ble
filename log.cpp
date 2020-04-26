6c436fbc2598db09ce413f833275a156e4b503c2
phooky <phooky@gmail.com>
2020-04-25 22:16:59 -0400

    improving debug information

--- log.cpp
+++ log.cpp
@@ -3,6 +3,7 @@
 #include <bluetooth/hci.h>
 #include "adv_packet.h"
 #include <iostream>
+#include <iomanip>
 #include <sstream>
 
 LogBuilder::LogBuilder(const std::string& logbase, const uint32_t interval, bool debug) : 
@@ -13,16 +14,26 @@ LogBuilder::~LogBuilder() {
     if (out.is_open()) out.close();
 }
 
+void writehex(std::ostream& o, uint8_t* data, size_t len) {
+    std::ios_base::fmtflags f(o.flags());;
+    o << std::setfill('0') << std::setw(2) << std::hex << (int)data[0];
+    for (auto i = 1; i < len; i++) o << ":" << (int)data[i];
+    o.flags(f);
+}
+
 void LogBuilder::log_report(uint8_t* report_data, size_t sz) {
     std::ostream& o = ostream();
     le_advertising_info* ad = (le_advertising_info*)report_data;
     EN_packet* p = (EN_packet*)ad->data;
     if (debug) {
-        o << std::dec << time(NULL) << ": got RPI " << std::hex << (int)p->data_rpi[0];
-        for (auto i = 1; i < 0x10; i++) o << ":" << (int)p->data_rpi[i];
+        o << std::dec << time(NULL) << " from ";
+        writehex(o, ad->bdaddr.b, 6);
+        o << " (type " << std::hex << ad->bdaddr_type << std::endl;
+        o << "    got RPI ";
+        writehex(o, p->data_rpi, 16);
         if (sz == 31) {
-            o << " and AEM " << (int)p->data_aem[0];
-            for (auto i = 1; i < 4; i++) o << ":" << (int)p->data_aem[i];
+            o << " and AEM ";
+            writehex(o, p->data_aem, 4);
         }
         o << std::endl;
     } else {
